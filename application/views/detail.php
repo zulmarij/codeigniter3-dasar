88aee3029b76c6084dc1c8a93cf8acbb9d6e0659
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-16 21:47:40 +0700

    codeigniter3 dasar

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,7 +1,13 @@
 <?php $this->load->view('partials/header'); ?>
 
 <!-- Page Header -->
-<header class="masthead" style="background-image: url('<?php echo base_url() .'uploads/'. $blog->cover; ?>')">
+<?php
+if (empty($blog->cover))
+	$cover = base_url() . 'assets/img/post-bg.jpg';
+else
+	$cover = base_url() . 'uploads/' . $blog->cover;
+?>
+<header class="masthead" style="background-image: url('<?php echo $cover ?>')">
 	<div class="overlay"></div>
 	<div class="container">
 		<div class="row">
