c7604467e2fa75cb12c8e5ae23da3a30a034d194
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-16 15:45:43 +0700

    codeigniter3 dasar

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,2 +1,30 @@
-<h1><?php echo $blog->title ?></h1>
-<p><?php echo $blog->content ?></p>
+<?php $this->load->view('partials/header'); ?>
+
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('<?php echo base_url(); ?>/assets/img/post-bg.jpg')">
+	<div class="overlay"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-10 mx-auto">
+				<div class="post-heading">
+					<h1><?php echo $blog->title; ?></h1>
+					<span class="meta">Posted on <?php echo $blog->date; ?></span>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<!-- Post Content -->
+<article>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-10 mx-auto">
+				<p><?php echo $blog->content ?></p>
+			</div>
+		</div>
+	</div>
+</article>
+
+<hr>
+<?php $this->load->view('partials/footer'); ?>
