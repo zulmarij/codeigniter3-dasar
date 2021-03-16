83f1ad51acd36bc8b964499d00811a0eeaa83fd2
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-16 07:26:05 +0700

    codeigniter3 dasar

---
+++ application/views/percobaan.php
@@ -0,0 +1,5 @@
+<h1>Ini halam percobaan</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vel vitae autem voluptatibus. Autem unde iure, expedita minus, facilis necessitatibus laudantium voluptatem ratione voluptatum totam, consequatur voluptas ad asperiores corporis! </p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vel vitae autem voluptatibus. Autem unde iure, expedita minus, facilis necessitatibus laudantium voluptatem ratione voluptatum totam, consequatur voluptas ad asperiores corporis! </p>
