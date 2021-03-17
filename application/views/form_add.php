0b1671ba16a777c24500df4f7199e413e837fe76
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-17 10:37:31 +0700

    codeigniter3 dasar

--- application/views/form_add.php
+++ application/views/form_add.php
@@ -21,22 +21,27 @@
 				<?php echo validation_errors() ?>
 			</div>
 			<?php echo form_open_multipart();
+
 			echo '<div class="form-group">';
 			echo form_label('Judul', 'title');
 			echo form_input('title', null, 'class="form-control"');
 			echo '</div>';
+			
 			echo '<div class="form-group">';
 			echo form_label('URL', 'url');
 			echo form_input('url', null, 'class="form-control"');
 			echo '</div>';
+			
 			echo '<div class="form-group">';
 			echo form_label('Konten', 'content');
 			echo form_textarea('content', null, 'class="form-control"');
 			echo '</div>';
+			
 			echo '<div class="form-group">';
 			echo form_label('Cover', 'cover');
 			echo form_upload('cover', null, 'class="form-control"');
 			echo '</div>';
+			
 			echo form_submit('submit', 'Simpan Artikel', 'class="btn btn-primary"');
 			echo form_close(); ?>
 		</div>
